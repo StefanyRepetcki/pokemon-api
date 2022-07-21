44ad907aef2133cf9b97acec9b86eeffbb56f54e
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-07-21 20:27:41 -0300

    feat(Components): Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-if="loading" class="loading">
+        <div class="loading__icon"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        loading: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+.loading {
+    width: 100vw;
+    top: 0;
+    background: rgba(0, 0, 0, 0.8);
+    left: 0;
+    position: fixed;
+    display: flex;
+    z-index: 998;
+    height: 100%;
+
+    &__icon {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #2C2C2D;
+        margin: auto;
+        border-radius: 50%;
+        width: 60px;
+        height: 60px;
+        animation: spin 2s linear infinite;
+    }
+}
+</style>
+
