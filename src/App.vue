611f88fb342e85f34415f6a84dc2a3d4ad9131ae
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-07-21 20:30:34 -0300

    feat(config): package, babel vue js

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <menu-pokemon></menu-pokemon>
+    <router-view />
+  </div>
+</template>
+<style lang="scss" src="./sass/app.scss">
+</style>
+<script>
+import MenuPokemon from './components/MenuPokemon.vue';
+
+export default {
+  components: {
+    MenuPokemon,
+  },
+}
+</script>
