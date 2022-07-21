1a5ab0c62738893a57514934a2f0f7b0255ec78f
Stefany Repetcki <stefany.c.r@hotmail.com>
2022-07-21 20:30:50 -0300

    feat: views

---
+++ src/views/Description.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="description">
+    <description-pokemon :id="id"></description-pokemon>
+  </div>
+</template>
+
+<script>
+import DescriptionPokemon from '../components/DescriptionPokemon.vue';
+
+export default {
+  components: {
+    DescriptionPokemon
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+}
+</script>
---
+++ src/views/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="list">
+    <list-pokemon></list-pokemon>
+  </div>
+</template>
+
+<script>
+import ListPokemon from '../components/ListPokemon.vue';
+
+export default {
+  components: {
+    ListPokemon
+  }
+}
+</script>
